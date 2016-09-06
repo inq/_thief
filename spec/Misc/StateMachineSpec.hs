514cbacb5f780dbeba27afdfd0374c774e91ef2d
inkyu <gofiri@gmail.com>
2016-09-06 15:25:24 +0800

    applicative state machine

--- spec/Misc/StateMachineSpec.hs
+++ spec/Misc/StateMachineSpec.hs
@@ -9,14 +9,25 @@ spec = describe "StateMachine" $ do
   context "Basic" $ do
     it "parses single charactor" $ do
       let p = char 'x'
-          (f, _) = (runMore p) 'y'
-          (s, _) = (runMore p) 'x'
+          (f, _) = runMore p 'y'
+          (s, _) = runMore p 'x'
       f `shouldBe` Nothing
       s `shouldBe` Just 'x'
   context "Functor" $ do
     it "is a functor" $ do
       let p = (:[]) <$> char 'x'
-          (f, _) = (runMore p) 'y'
-          (s, _) = (runMore p) 'x'
+          (f, _) = runMore p 'y'
+          (s, _) = runMore p 'x'
       f `shouldBe` Nothing
       s `shouldBe` Just "x"
+  context "Applicative" $ do
+    it "process a string" $ do
+      let p0 = string "hello"
+          (_, p1) = runMore p0 'h'
+          (_, p2) = runMore p1 'e'
+          (_, p3) = runMore p2 'l'
+          (_, p4) = runMore p3 'l'
+          (r5, _) = runMore p4 'o'
+          (r6, _) = runMore p2 'h'
+      r5 `shouldBe` Just "hello"
+      r6 `shouldBe` Nothing
--- src/Misc/StateMachine.hs
+++ src/Misc/StateMachine.hs
@@ -1,6 +1,7 @@
 module Misc.StateMachine
   ( StateMachine(..)
   , char
+  , string
   ) where
 
 data StateMachine a
@@ -13,13 +14,33 @@ data StateMachine a
 
 instance Functor StateMachine where
   fmap f (More action) = More $ \c' ->
-    let (re, ne) = action c'
-    in  (f <$> re, f <$> ne)
+    let (result, next) = action c'
+    in  (f <$> result, f <$> next)
   fmap f (Func a) = Func $ f a
   fmap _ Success = Success
   fmap _ Failure = Failure
+
+instance Applicative StateMachine where
+  pure f = Func f
+  Func a <*> Func b = Func (a b)
+  Func a <*> More actionB = More $ \c' ->
+    let (resultB, nextB) = actionB c'
+    in  (a <$> resultB, a <$> nextB)
+  More actionA <*> Func b = More $ \c' ->
+    let (resultA, nextA) = actionA c'
+    in  (resultA <*> pure b, nextA <*> pure b)
+  More actionA <*> More actionB = More $ \c' ->
+    let Success ||| Just x = pure x
+        a       ||| _      = a
+        (resultA, nextA) = actionA c'
+    in  (Nothing, (nextA ||| resultA) <*> More actionB)
+  _ <*> _ = Failure
 
 char :: Char -> StateMachine Char
 char c = More $ \c' -> if c == c'
   then (Just c, Success)
   else (Nothing, Failure)
+
+string :: String -> StateMachine String
+string (c:cs) = pure (:) <*> char c <*> string cs
+string [] = pure []
