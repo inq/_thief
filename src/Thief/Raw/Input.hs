f8ebaba46d900343ca8cc95ca5cb03655130f7b7
inkyu <gofiri@gmail.com>
2016-09-09 02:24:44 +0800

    remove Status module

--- src/Thief/Raw/Input.hs
+++ src/Thief/Raw/Input.hs
@@ -4,13 +4,14 @@ module Thief.Raw.Input
   ) where
 
 import Control.Concurrent.Chan (Chan, writeChan)
+import Control.Applicative ((<|>))
 import System.IO
   ( hSetBuffering, hSetEcho
   , stdin, stdout
   , BufferMode(NoBuffering))
-import Misc (StateMachine(..))
+import Misc (StateMachine(..), string, char, integer, anyChar)
 import Thief.Raw.Result (Result)
-import Thief.Status (initialState)
+import qualified Thief.Raw.Result as Res
 
 initialize :: IO ()
 -- ^ Initialize the input
@@ -18,6 +19,20 @@ initialize = do
     hSetBuffering stdout NoBuffering
     hSetBuffering stdin NoBuffering
     hSetEcho stdin False
+
+csi :: StateMachine String
+csi = string "\ESC["
+
+initialState :: StateMachine Res.Result
+-- ^ The initial state of the StateMachine
+initialState
+  =   Res.Arrow Res.AUp    <$ csi <* char 'A'
+  <|> Res.Arrow Res.ADown  <$ csi <* char 'B'
+  <|> Res.Arrow Res.ARight <$ csi <* char 'C'
+  <|> Res.Arrow Res.ALeft  <$ csi <* char 'D'
+  <|> Res.Pair <$ csi <*> integer <* char ';' <*> integer <* char 'R'
+  <|> Res.Trio <$ csi <*> integer <* char ';' <*> integer <* char ';' <*> integer <* char 't'
+  <|> Res.Char <$> anyChar
 
 inputLoop :: Chan Result -> IO ()
 -- ^ Input from the terminal
